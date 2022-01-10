a4a62b3a407f42276d9473c5485e312321f22233
DevMaximiliano <mfarias@encodesa.com.ar>
2022-01-10 14:53:43 -0300

    Se programa la eliminacion de tareas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { isEmpty } from 'lodash';
+import { isEmpty, size } from 'lodash';
 import React, {useState} from 'react';
 import shortid from 'shortid';
 
@@ -29,6 +29,13 @@ function App() {
     setTask('');
   }
 
+
+  const deleteTask = (id) =>{
+    const filterTask = tasks.filter(task => task.id !== id)
+
+    setTasks(filterTask)
+  }
+
   return (
     <div className='container mt-5'>
       <h1> Tareas</h1>
@@ -36,20 +43,35 @@ function App() {
       <div className='row'>
         <div className='col-8'>
           <h4 className='text-center'>Lista de Tareas</h4>
-          <ul className='list-group'>
+          {
+            size(tasks) === 0 ? (
+              <h5 className='text-center mt-3'>No hay tareas programadas.</h5>
+            ) : (
+              <ul className='list-group'>
             {
 
               tasks.map((task) => (
                 <li className='list-group-item' key={task.id}>
                 <span className='lead'>{task.name}</span>
-                <button className='btn btn-danger btn-sm float-right mx-2'>Eliminar</button>
-                <button className='btn btn-warning btn-sm float-right'>Editar</button>
+                <button 
+                className='btn btn-danger btn-sm float-right mx-2'
+                onClick={() => deleteTask(task.id)}
+                >Eliminar
+                </button>
+                <button 
+                className='btn btn-warning btn-sm float-right'
+                >Editar
+                </button>
                 </li>
               ))
 
               
             }
-          </ul>
+            </ul>
+            )
+            
+          }
+          
         </div>
         <div className='col-4'>
         <h4 className='text-center'>Formulario de Tareas</h4>
